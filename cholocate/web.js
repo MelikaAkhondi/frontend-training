dc90882fc29c9ba213a52316e12c96e65ec662b9
unknown <melikaa380@gmail.com>
2025-03-02 12:42:25 +0330

    a chocholate web page is implemented

---
+++ cholocate/web.js
@@ -0,0 +1,11 @@
+// window.addEventListener('scroll', () => {
+  // const scrollPosition = window.scrollY;
+
+//   if (scrollPosition < 500) {
+//     document.body.style.backgroundImage = "url('images/background.png')";
+//   } else if (scrollPosition >= 500 && scrollPosition < 1000) {
+//     document.body.style.backgroundImage = "url('images/file')" ;
+//   } else {
+//     document.body.style.backgroundImage = "url('images/background2.png')";
+//   }
+// });
